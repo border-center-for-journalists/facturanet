492f9ec5c93a5da3bbcc8f2a7b8304567f5ce66f
erick <erick@spaceshiplabs.com>
2020-08-13 15:25:30 -0500

    api url

--- src/helpers/FetchNode.js
+++ src/helpers/FetchNode.js
@@ -1,6 +1,6 @@
 const fetchNode = async (node, params = {}) => {
-  const url = new URL("http://localhost:1337/" + node);
-  // const url = new URL("https://facturanet-api.herokuapp.com/" + node);
+  // const url = new URL("http://localhost:1337/" + node);
+  const url = new URL("https://facturanet-api.herokuapp.com/" + node);
   Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
   const res = await fetch(url);
   return res.json();
