645ef92d16e3c0f6076674066dfcdaf002395239
El Sonny <sonny@spaceshiplabs.com>
2021-02-01 18:49:06 +0100

    change to prod api

--- src/helpers/FetchNode.js
+++ src/helpers/FetchNode.js
@@ -1,6 +1,6 @@
 const fetchNode = async (node, params = {}) => {
-  const url = new URL("http://localhost:1337/" + node);
-  //const url = new URL("https://facturanet-api.herokuapp.com/" + node);
+  //const url = new URL("http://localhost:1337/" + node);
+  const url = new URL("https://facturanet-api.herokuapp.com/" + node);
   Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
   return fetch(url).then(res => res.json());
 }
